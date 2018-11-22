160cbb06ab08a6661d3ac67806d3ed4e6a6d2b11
liujiabei <liujiabei@xunlei.com>
2018-11-22 19:20:13 +0800

    eee

---
+++ hhrpc-demo/hhrpc-demo-producer/src/main/java/com/ljb/hhrpc/demo/producer/HhrpcDemoProducerApplication.java
@@ -0,0 +1,12 @@
+package com.ljb.hhrpc.demo.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhrpcDemoProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhrpcDemoProducerApplication.class, args);
+    }
+}
